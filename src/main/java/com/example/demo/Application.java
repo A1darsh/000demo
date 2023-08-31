593e338a578f0a679324a06f6f6caa1b5e1ee983
Adarsh <adarshmishra5432@gmail.com>
2023-08-31 14:44:03 +0530

    task done

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		//i have to do this first;
+		//i have done this task;
 	}
 
 }
